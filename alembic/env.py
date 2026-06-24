1bf82965473673c74c7d66c1517e8aa8c2215d13
Sarvar <uzcardlite@gmail.com>
2026-06-25 02:36:38 +0500

    fix env.py database url

--- alembic/env.py
+++ alembic/env.py
@@ -1,31 +1,31 @@
+import os
 from logging.config import fileConfig
-from sqlalchemy import pool
-from sqlalchemy.engine import Connection
+from sqlalchemy import pool, create_engine
 from alembic import context
 from app.models import Base
 
 config = context.config
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
 target_metadata = Base.metadata
 
+def get_url():
+    return os.environ.get("DATABASE_URL", config.get_main_option("sqlalchemy.url"))
 
 def run_migrations_offline() -> None:
-    url = config.get_main_option("sqlalchemy.url")
+    url = get_url()
     context.configure(url=url, target_metadata=target_metadata, literal_binds=True)
     with context.begin_transaction():
         context.run_migrations()
 
-
 def run_migrations_online() -> None:
-    from sqlalchemy import create_engine
-    connectable = create_engine(config.get_main_option("sqlalchemy.url"))
+    connectable = create_engine(get_url(), poolclass=pool.NullPool)
     with connectable.connect() as connection:
         context.configure(connection=connection, target_metadata=target_metadata)
         with context.begin_transaction():
             context.run_migrations()
-
 
 if context.is_offline_mode():
     run_migrations_offline()
